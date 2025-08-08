19ee476ef57c7e08a55f4c5d58114f4a710a17d5
eliob <elio.bucher10@gmail.com>
2025-08-08 13:15:31 +0200

    2. Versuch

--- src/Main.java
+++ src/Main.java
@@ -32,7 +32,7 @@ public class Main {
                     "Als Spieler gibst du in der Konsole eine Farbfolge ein (durch ein Leerzeichen getrennt.)\n" +
                     "\n" +
                     "Das Spiel gibt die eine Rückmeldung (X ein Farbe ist korrekt und am richtigen Platz, 0 eine Farbe ist Korekkt aber steht am falschen Platz. _ nicht in der geheimen Farbfolge.)\n" +
-                     "\n" +
+                      "\n" +
                     "Das Spiel endet, wenn du die richtige Reihenfolge innert 12 Versuchen errätst.\n" +
                     "\n" +
                     "Ansonsten hast du nach 12 Versuchen verloren.");
