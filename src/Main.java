50d51e2863a1ba62d2df25f88c24a6b038fd3d57
eliob <elio.bucher10@gmail.com>
2025-08-08 08:28:00 +0200

    1. Versuch

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,37 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner spielregel = new Scanner(System.in);
+
+        System.out.println("Kennst du das Spiel MasterMind?");
+        String entscheidung1 = spielregel.nextLine();
+
+        while (entscheidung1 == "ja") {
+            System.out.println("Die möglichen Farben sind: Rot, Blau, Grün, Gelb, Pink, Schwarz, Grau und Orange");
         }
+
+        while (entscheidung1 == "nein") {
+            System.out.println("Spielregeln:\n" +
+                    "\n" +
+                    "Es gibt eine versteckte Farbenfolge, die aus vier Farben besteht\n" +
+                    "\n" +
+                    "Dabei dürfen die Farben doppelt vorkommen.\n" +
+                    "\n" +
+                    "Es gibt die Farben: Blau, Rot, Grün, Gelb, Orange, Violet\n" +
+                    "\n" +
+                    "Als Spieler gibst du in der Konsole eine Farbfolge ein (durch ein Leerzeichen getrennt.)\n" +
+                    "\n" +
+                    "Das Spiel gibt die eine Rückmeldung (X ein Farbe ist korrekt und am richtigen Platz, 0 eine Farbe ist Korekkt aber steht am falschen Platz. _ nicht in der geheimen Farbfolge.)\n" +
+                    "\n" +
+                    "Das Spiel endet, wenn du die richtige Reihenfolge innert 12 Versuchen errätst.\n" +
+                    "\n" +
+                    "Ansonsten hast du nach 12 Versuchen verloren.");
+        }
+
+        System.out.println("Gib entweder ja oder nein ein!");
     }
+
 }
