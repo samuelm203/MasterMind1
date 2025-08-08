c1fdcadc4219d37da02d52efec409e10a4b071ee
eliob <elio.bucher10@gmail.com>
2025-08-08 11:33:42 +0200

    2. Versuch

--- src/Main.java
+++ src/Main.java
@@ -5,22 +5,29 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner spielregel = new Scanner(System.in);
+        Random generator = new Random();
+        int counter = 0;
+        int richtigerort = 0;
+        int richtigefarben = 0;
 
-        System.out.println("Kennst du das Spiel MasterMind?");
-        String entscheidung1 = spielregel.nextLine();
+        String[] farben = {"Rot", "Blau", "Grün", "Gelb", "Pink", "Schwarz", "Grau", "Orange"};
 
-        while (entscheidung1 == "ja") {
+
+        System.out.println("Kennst du das Spiel MasterMind? Für ja = 1/ Für nein = 2 ");
+        int entscheidung1 = spielregel.nextInt();
+
+        if (entscheidung1 == 1) {
             System.out.println("Die möglichen Farben sind: Rot, Blau, Grün, Gelb, Pink, Schwarz, Grau und Orange");
         }
 
-        while (entscheidung1 == "nein") {
+        if (entscheidung1 == 2) {
             System.out.println("Spielregeln:\n" +
                     "\n" +
                     "Es gibt eine versteckte Farbenfolge, die aus vier Farben besteht\n" +
                     "\n" +
                     "Dabei dürfen die Farben doppelt vorkommen.\n" +
                     "\n" +
-                    "Es gibt die Farben: Blau, Rot, Grün, Gelb, Orange, Violet\n" +
+                    "Es gibt die Farben: Rot, Blau, Grün, Gelb, Pink, Schwarz, Grau und Orange\n" +
                     "\n" +
                     "Als Spieler gibst du in der Konsole eine Farbfolge ein (durch ein Leerzeichen getrennt.)\n" +
                     "\n" +
@@ -31,7 +38,33 @@ public class Main {
                     "Ansonsten hast du nach 12 Versuchen verloren.");
         }
 
-        System.out.println("Gib entweder ja oder nein ein!");
-    }
+        String farbenzufaellig1 = farben[generator.nextInt(farben.length)];
 
+        String farbenzufaellig2 = farben[generator.nextInt(farben.length)];
+
+        String farbenzufaellig3 = farben[generator.nextInt(farben.length)];
+
+        String farbenzufaellig4 = farben[generator.nextInt(farben.length)];
+
+            while (counter <= 12) {
+                counter++;
+
+                System.out.println("Gib deine erste Farbe ein!");
+                String eingabeeins = spielregel.nextLine();
+
+                String eingabezwei = spielregel.nextLine();
+                System.out.println("Gib deine zweite Farbe ein!");
+
+                String eingabedrei = spielregel.nextLine();
+                System.out.println("Gib deine dritte Farbe ein!");
+
+                String eingabevier = spielregel.nextLine();
+                System.out.println("Gib deine vierte Farbe ein!");
+
+
+            }
+
+    }
 }
+
+
